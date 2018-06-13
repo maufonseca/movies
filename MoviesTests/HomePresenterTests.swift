56b69773bfa9acda729bb64ff7d65516dd250783
Maurício Fonseca <mauricio.fonseca@ringa.com.br>
2018-06-13 18:58:03 -0300

    Add unit tests to HomePresenter and Movie parser

---
+++ MoviesTests/HomePresenterTests.swift
@@ -0,0 +1,60 @@
+//
+//  ResponseErrorTests.swift
+//  MoviesTests
+//
+//  Created by Ringa Sd on 13/06/2018.
+//  Copyright © 2018 Mauricio Fonseca. All rights reserved.
+//
+
+import XCTest
+@testable import Movies
+
+class HomePresenterTests: XCTestCase {
+  
+  
+  //Stub classes (mock)
+  class MockViewController: HomeViewController {
+    var showedMessage = false //Will be true when showMessage() got called
+    var updatedArray = false //Will be true when updateMovieList() got called
+    override func showMessage(message: String) {
+      showedMessage = true
+    }
+    override func updateMovieList(array: Array<Movie>) {
+      updatedArray = true
+    }
+  }
+  
+  //Objects used for testing
+  var mockViewController : MockViewController?
+  var presenterUnderTest : HomePresenter?
+  var moviesRequester : MoviesRequester?
+  
+  override func setUp() {
+    super.setUp()
+    mockViewController = MockViewController.init()
+    presenterUnderTest = HomePresenter.init(controller: mockViewController!)
+    moviesRequester = MoviesRequester.init(presenter: presenterUnderTest!)
+  }
+  
+  override func tearDown() {
+    moviesRequester = nil
+    presenterUnderTest = nil
+    mockViewController = nil
+    super.tearDown()
+  }
+  
+  func testErrorMessage() {
+    //When the movies requester pass an error to the presenter,
+    //assert the Home viewController showed the error message
+    moviesRequester?.presenter.gotError(message: "erro")
+    XCTAssertTrue(mockViewController!.showedMessage)
+  }
+  
+  func testSuccessResponse() {
+    //When the requester pass a Movies array to the presenter
+    //Assert that the movies list will be updated in the viewController
+    moviesRequester?.presenter.updateMoviesArray(array: [])
+    XCTAssertTrue(mockViewController!.updatedArray)
+  }
+  
+}
--- MoviesTests/MovieModelTests.swift
+++ MoviesTests/MovieModelTests.swift
@@ -38,11 +38,20 @@ class MovieModelTests: XCTestCase {
     
   }
   
-  func testPerformanceExample() {
-    // This is an example of a performance test case.
-    self.measure {
-      // Put the code you want to measure the time of here.
-    }
+  func testDictionaryParse() {
+    
+    let dict : Dictionary<String, Any> = ["vote_count":439,"id":351286,"video":false,"vote_average":6.7,"title":"Jurassic World: Reino Ameaçado","popularity":288.186315,"poster_path":"/nR6LhLKk5BsholuIHIAGemMKPKy.jpg","original_language":"en","original_title":"Jurassic World: Fallen Kingdom","genre_ids":[28,12,878],"backdrop_path":"/t0Kn7twXduHeFhOpTW2Gncu9l5F.jpg","adult":false,"overview":"Quatro anos após o fechamento do Jurassic Park, um vulcão prestes a entrar em erupção põe em risco a vida na ilha Nublar. No local não há mais qualquer presença humana, com os dinossauros vivendo livremente. Diante da situação, é preciso tomar uma decisão: deve-se retornar à ilha para salvar os animais ou abandoná-los para uma nova extinção? Decidida a resgatá-los, Claire (Bryce Dallas Howard) convoca Owen (Chris Pratt) a retornar à ilha com ela.","release_date":"2018-06-06"]
+    
+    let nsDict = dict as NSDictionary
+    
+    movieUnderTest = Movie.init(fromDictionary: nsDict)
+    XCTAssert(movieUnderTest?.id == 351286)
+    XCTAssert(movieUnderTest?.title == "Jurassic World: Reino Ameaçado")
+    XCTAssert(movieUnderTest?.bookmarked == false)
+    XCTAssert(movieUnderTest?.getReleaseYear()=="2018")
+    XCTAssert(movieUnderTest?.overview == "Quatro anos após o fechamento do Jurassic Park, um vulcão prestes a entrar em erupção põe em risco a vida na ilha Nublar. No local não há mais qualquer presença humana, com os dinossauros vivendo livremente. Diante da situação, é preciso tomar uma decisão: deve-se retornar à ilha para salvar os animais ou abandoná-los para uma nova extinção? Decidida a resgatá-los, Claire (Bryce Dallas Howard) convoca Owen (Chris Pratt) a retornar à ilha com ela.")
   }
+  
+  
   
 }
